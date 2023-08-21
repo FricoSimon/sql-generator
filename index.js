95f59be39508c23459f7ddea7114a7f4cbc3e420
FricoSimon <simonfriko@gmail.com>
2023-08-21 12:40:33 +0700

    add select from a column from 1 table

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ import inquirer from "inquirer";
 import selectSQL from "./prompt/selectSQL.js";
 
 const choices = ["Select SQL", "Insert SQL", "Delete SQL"];
-const tables = [1, 2, 3];
 
 inquirer
-    .prompt([{
+    .prompt({
         type: "rawlist",
         name: "query",
         message: "What do you want to create?",
         choices: choices
-    }])
+    })
     .then(answer => {
         if (answer.query === "Select SQL") {
             selectSQL();
--- prompt/selectSQL.js
+++ prompt/selectSQL.js
@@ -1,26 +1,63 @@
 import inquirer from "inquirer";
 
 const tables = [1, 2, 3];
+const columns = ["all", "specific"];
 
 function selectSQL() {
     inquirer
-        .prompt([{
-            type: "rawlist",
-            name: "select_table",
-            message: "How many tables do you want to select?",
-            choices: tables
-        }])
-        .then(answer => {
+        .prompt([
+            {
+                type: "rawlist",
+                name: "select_table",
+                message: "How many tables do you want to select?",
+                choices: tables,
+            },
+        ])
+        .then((tableAnswer) => {
             inquirer
-                .prompt([{
-                    type: "input",
-                    name: "table1",
-                    message: "First table name: ",
-                }])
-                .then(answer => {
-                    console.log(answer.table1);
-                })
-        })
+                .prompt([
+                    {
+                        type: "input",
+                        name: "table1",
+                        message: "First table name: ",
+                    },
+                    {
+                        type: "rawlist",
+                        name: "column",
+                        message: "Select all columns or specific columns?",
+                        choices: columns,
+                    },
+                ])
+                .then((columnAnswer) => {
+                    if (columnAnswer.column === "all") {
+                        console.log(`SELECT * FROM ${columnAnswer.table1}`);
+                    } else if (columnAnswer.column === "specific") {
+                        inquirer
+                            .prompt([
+                                {
+                                    type: "number",
+                                    name: "number_of_columns",
+                                    message: "How many columns do you want to select?",
+                                },
+                            ])
+                            .then((numberColumnAnswer) => {
+                                if (numberColumnAnswer.number_of_columns === 1) {
+                                    inquirer
+                                        .prompt([
+                                            {
+                                                type: "input",
+                                                name: "column1",
+                                                message: "First column name: ",
+                                            },
+                                        ])
+                                        .then((column1Answer) => {
+                                            console.log(`SELECT ${column1Answer.column1} FROM ${columnAnswer.table1}`);
+                                        });
+                                }
+                            });
+                    }
+                });
+        });
 }
 
 export default selectSQL;
